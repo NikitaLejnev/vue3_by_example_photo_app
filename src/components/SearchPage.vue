e72d5352bc54cef0de232d18d50df52b289b1692
NikitaLejnev <catassasin331@gmail.com>
2022-01-17 14:26:41 +0300

    Added SearchPage.vue

---
+++ src/components/SearchPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Search</h1>
+    <form @submit.prevent="submit">
+      <div>
+        <label for="name">Keyword</label>
+        <br />
+        <input
+          type="text"
+          v-model="keyword"
+          name="keyword"
+          id="keyword"
+          class="form-field"
+        />
+      </div>
+      <div>
+        <input type="submit" value="Search" />
+      </div>
+    </form>
+    <div v-for="p of photos" class="row" :key="p.id">
+      <div>
+        <img :src="p.photoFile" />
+      </div>
+      <div>{{ p.name }}</div>
+      <div>{{ p.description }}</div>
+    </div>
+  </div>
+</template>
