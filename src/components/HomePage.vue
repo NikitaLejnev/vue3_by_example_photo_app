5723ae9603a64f79085012e87cb54b99baf5e812
NikitaLejnev <catassasin331@gmail.com>
2022-01-17 13:33:28 +0300

    Started writing HomePage.vue

---
+++ src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Photos</h1>
+    <button @click="load">Refresh</button>
+    <div class="row">
+      <div>Name</div>
+      <div>Photo</div>
+      <div>Description</div>
+      <div>Actions</div>
+    </div>
+  </div>
+</template>
+ <script>
+ import axios from "axios";
+ import { APIURL } from '../constant';
+
+ export default {
+   data() {
+     return {
+       photos: [],
+     };
+   },
+   methods: {}
+ }
+ </script>
+ 
+ <style scoped>
+ 
+ </style>
