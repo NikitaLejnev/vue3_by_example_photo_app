f7f0d98c4d072605ff15b302e9d5460d756a3af8
NikitaLejnev <catassasin331@gmail.com>
2022-01-17 14:03:18 +0300

    Added <style> to HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,38 +8,61 @@
       <div>Description</div>
       <div>Actions</div>
     </div>
+    <div v-for="p of photos" class="row" :key="p.id">
+      <div>
+        <img :src="p.photoFile" />
+      </div>
+      <div>{{ p.name }}</div>
+      <div>{{ p.description }}</div>
+      <div>
+        <button @click="edit(p.id)">Edit</button>
+        <button @click="deletePhoto(p.id)">Delete</button>
+      </div>
+    </div>
   </div>
 </template>
- <script>
- import axios from "axios";
- import { APIURL } from '../constant';
+<script>
+import axios from "axios";
+import { APIURL } from "../constant";
 
- export default {
-   data() {
-     return {
-       photos: [],
-     };
-   },
-   methods: {
-     async load() {
-       const { data } = await axios.get(`${APIURL}/photos`);
-       this.photos = data;
-     },
-     edit(id) {
-       this.$router.push({ path: `/edit-photo-form/${id}` });
-     },
-     async deletePhoto(id) {
-       await axios.delete(`${APIURL}/photos/${id}`);
-       this.photos = [];
-       this.load();
-     },
-   },
-   beforeMount() {
-     this.load();
-   },
- };
- </script>
- 
- <style scoped>
- 
- </style>
+export default {
+  data() {
+    return {
+      photos: [],
+    };
+  },
+  methods: {
+    async load() {
+      const { data } = await axios.get(`${APIURL}/photos`);
+      this.photos = data;
+    },
+    edit(id) {
+      this.$router.push({ path: `/edit-photo-form/${id}` });
+    },
+    async deletePhoto(id) {
+      await axios.delete(`${APIURL}/photos/${id}`);
+      this.photos = [];
+      this.load();
+    },
+  },
+  beforeMount() {
+    this.load();
+  },
+};
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.row div {
+  width: 25%;
+}
+
+.row img {
+  width: 100px;
+}
+</style>
