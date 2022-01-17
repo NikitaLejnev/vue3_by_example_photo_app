4e7ddd261d113fdd3a6d6180747bd13bd7ea57a3
NikitaLejnev <catassasin331@gmail.com>
2022-01-17 08:59:10 +0300

    Added Prettier Completed <template> in PhotoFormPage.vue

---
+++ src/components/PhotoFormPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form">
+    <h1>{{ $route.params.id ? "Edit" : "Add" }} Photo</h1>
+    <form @submit.prevent="submit">
+      <div>
+        <label for="name">Name</label>
+        <br />
+        <input
+          type="text"
+          v-model="form.name"
+          name="name"
+          id="name"
+          class="form-field"
+        />
+      </div>
+      <div>
+        <label for="description">Description</label>
+        <br />
+        <textarea
+          v-model="form.description"
+          name="description"
+          id="description"
+          class="form-field"
+        ></textarea>
+      </div>
+      <div>
+        <label for="dateTaken">Date Taken</label>
+        <br />
+        <input
+          type="datetime-local"
+          name="dateTaken"
+          id="dateTaken"
+          v-model="form.dateTaken"
+        />
+      </div>
+      <div>
+        <label for="photoFile">Photo</label>"
+        <br />
+        <input type="file" name="photoFile" id="photoFile" @change="onChange" />
+        <br />
+        <img :src="form.photoFile" id="photo-preview" />
+      </div>
+      <div>
+        <input type="submit" />
+      </div>
+    </form>
+  </div>
+</template>
